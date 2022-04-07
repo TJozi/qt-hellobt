eb494b5a1f9097b818c11948bcead7781f2ce6fc
Thien Jozipovic <thien.tranjozipovic@students.hevs.ch>
2022-04-07 11:43:22 +0200

    Started web game.
    Connection ok, buttons actions ok, setting btn led from webpage

---
+++ Web/scripts/connect.js
@@ -0,0 +1,40 @@
+var client = new Paho.MQTT.Client("mqtt-ws.sdi.hevs.ch", 80, "/ws", "rgubsdflkoihisohieg");
+
+client.onConnectionLost = function (responseObject) {
+    if (responseObject.errorCode !== 0) {
+        console.error("Lost connection:" + responseObject.errorMessage);
+    } else {
+        console.log("Connection closed.");
+    }
+}
+
+client.onMessageArrived = function (message) {
+    console.log("Got message: topic=" + message.destinationName + ', payload=' +
+        message.payloadString);
+        
+    processMessage(message);
+    //processMessage(message.destinationName, message.payloadString);
+}
+
+client.connect({
+    userName: 'sdi09',
+    password: '18681422d3fc90fcb3bed30757aebb3f',
+    keepAliveInterval: 30,
+    cleanSession: true,
+
+    onSuccess: function () {
+        setClient(client);
+
+        console.log("Connected.");
+        client.subscribe("sdi09/status");
+        client.subscribe("sdi09/+/button");
+        client.send('sdi09/D5:2F:7E:30:10:5A/led', JSON.stringify({
+            red: 0,
+            green: 0,
+            blue: 255
+        }));
+    },
+    onFailure: function () {
+        console.error("Failed to connect.");
+    }
+});
---
+++ Web/scripts/func.js
@@ -0,0 +1,37 @@
+var client;
+
+function switchColor(col)
+{
+    var color;
+    var square = document.getElementById("sqr");
+
+    square.style.backgroundColor = col;
+    
+}
+
+function processMessage(message)
+{
+
+    //Buttons processing
+    if(message.destinationName == "sdi09/D5:2F:7E:30:10:5A/button")
+        switchColor("rgb(139, 201, 255)");
+    else if(message.destinationName == "sdi09/FD:17:0C:19:6A:F7/button")
+        switchColor("rgb(235, 118, 113)");
+
+    if(message.payloadString == "false")
+        switchColor('white');
+}
+
+function setClient(clt)
+{
+    client = clt;
+}
+
+function btnPressed()
+{
+    client.send('sdi09/D5:2F:7E:30:10:5A/led', JSON.stringify({
+        red: 0,
+        green: 0,
+        blue: 255
+    }));
+}
